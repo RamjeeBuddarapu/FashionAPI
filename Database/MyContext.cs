5d47e85f0d865ea98551d53e95d8d6002e9050a3
Ramjee Buddarapu <Ramjee Buddarapu@MSI>
2023-11-21 12:32:52 +0530

    Add project files.

---
+++ Database/MyContext.cs
@@ -0,0 +1,22 @@
+﻿using FashionHexa.Entities;
+using System.Collections.Generic;
+namespace FashionHexa.Database
+{
+    public class MyContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product>? Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Role>? Roles { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Seller> Sellers { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Availability> Availabilities { get; set; }
+        public DbSet<UserRatings> UserRatings { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data source = MSI/DATAMODEL; Initial Catalog=EcomHexa ;User Id=Ramjee;Password=1234;TrustServerCertificate=true");
+        }
+    }
+}
